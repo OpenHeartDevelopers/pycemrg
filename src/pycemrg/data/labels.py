7b7448e68b038b32a8eecc49ebde93d2f3f255e8
Jose Alonso Solis Lemus <jsolisle@ic-czc52277x7>
2026-01-07 12:50:12 +0000

    labels_get_tag_string

--- src/pycemrg/data/labels.py
+++ src/pycemrg/data/labels.py
@@ -1,8 +1,10 @@
 # src/pycemrg/data/labels.py
 
 import yaml
+import itertools
 from pathlib import Path
 from typing import Union, List, Set
+
 
 class LabelManager:
     """
@@ -11,6 +13,7 @@ class LabelManager:
     This class reads a label manifest and provides utilities to translate
     between human-readable names, groups, and their integer values.
     """
+
     def __init__(self, config_path: Union[str, Path]):
         """
         Initializes the LabelManager by loading the configuration file.
@@ -20,14 +23,16 @@ class LabelManager:
         """
         config_file = Path(config_path)
         if not config_file.exists():
-            raise FileNotFoundError(f"Label configuration file not found at: {config_file}")
+            raise FileNotFoundError(
+                f"Label configuration file not found at: {config_file}"
+            )
 
-        with open(config_file, 'r') as f:
+        with open(config_file, "r") as f:
             config = yaml.safe_load(f)
 
-        self._labels = config.get('labels', {})
-        self._groups = config.get('groups', {})
-        
+        self._labels = config.get("labels", {})
+        self._groups = config.get("groups", {})
+
         self._value_to_name = {v: k for k, v in self._labels.items()}
 
     def get_value(self, name: str) -> int:
@@ -35,7 +40,7 @@ class LabelManager:
         if name not in self._labels:
             raise KeyError(f"Label name '{name}' not found.")
         return self._labels[name]
-    
+
     def get_name(self, value: int) -> str:
         """Translates an integer value (e.g., 1) to its human-readable name."""
         if value not in self._value_to_name:
@@ -62,9 +67,15 @@ class LabelManager:
                     f"Label or group name '{name}' not found. "
                     f"Available keys are: {available_keys}"
                 )
-        
+
         return sorted(list(final_values))
-    
-    def get_tags_string(self, names: List[str], separator: str = ',') -> str :
-        list_of_values = self.get_values_from_names(names) 
+
+    def get_tags_string(self, names: List[str], separator: str = ",") -> str:
+        if any(isinstance(i, list) for i in names):
+            flat_names = list(itertools.chain.from_iterable(names))
+        else:
+            flat_names = names
+
+        list_of_values = self.get_values_from_names(flat_names)
         return separator.join(list_of_values)
+
