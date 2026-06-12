9e3893c6a5d71e5585c20039a9d20c9992361c64
Jose Alonso Solis Lemus <jsolisle@ic-czc52277x7>
2026-06-12 14:35:13 +0100

    templates_update

---
+++ src/pycemrg/files/templates/__init__.py
@@ -0,0 +1,3 @@
+# Marker file making `templates` an importable subpackage so its data files
+# (e.g. *.template) are discovered by setuptools and accessible via
+# importlib.resources.files(templates).
